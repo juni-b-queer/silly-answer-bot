1b08a62eca193423ea939ef15cacdb5aa6506ed7
Juni! <june@juniper-bender.tech>
2024-04-05 00:01:54 -0500

    split up commands to different accounts

--- src/index.ts
+++ src/index.ts
@@ -9,35 +9,56 @@ import {
 } from "bsky-event-handlers";
 import {MagicEightBallHandler} from "./handler/MagicEightBall.ts";
 
-let lieDetectorAgentDetails: AgentDetails = {
+let magic8BallAgentDetails: AgentDetails = {
     name: "silly-reply-bot",
     did: undefined,
-    handle: <string>Bun.env.BOT_BSKY_HANDLE,
-    password: <string>Bun.env.BOT_BSKY_PASSWORD,
+    handle: <string>Bun.env.MAGIC_BOT_BSKY_HANDLE,
+    password: <string>Bun.env.MAGIC_BOT_BSKY_PASSWORD,
     sessionData: undefined,
     agent: undefined
 }
+
+let lieDetectorAgentDetails: AgentDetails = {
+    name: "silly-reply-bot",
+    did: undefined,
+    handle: <string>Bun.env.LIE_BOT_BSKY_HANDLE,
+    password: <string>Bun.env.LIE_BOT_BSKY_PASSWORD,
+    sessionData: undefined,
+    agent: undefined
+}
+magic8BallAgentDetails = createAgent(magic8BallAgentDetails)
 lieDetectorAgentDetails = createAgent(lieDetectorAgentDetails)
 
 let lieDetectorHandlerController: HandlerController;
+let magic8BallHandlerController: HandlerController;
 
 
 async function initialize() {
 
+    console.log("Initializing");
     lieDetectorAgentDetails = await authenticateAgent(lieDetectorAgentDetails)
+    magic8BallAgentDetails = await authenticateAgent(magic8BallAgentDetails)
 
     lieDetectorHandlerController = new HandlerController(lieDetectorAgentDetails, [
         LieDetectorHandler,
+        new GoodBotHandler(),
+        new BadBotHandler()
+        // TestHandler
+    ], false)
+
+    magic8BallHandlerController = new HandlerController(magic8BallAgentDetails, [
         MagicEightBallHandler,
         new GoodBotHandler(),
         new BadBotHandler()
         // TestHandler
     ], false)
 
+
     debugLog("INIT", 'Initialized!', 'warn')
 }
 
 try {
+    console.log("starting")
     await initialize();
 } catch (e) {
     setTimeout(async function () {
@@ -47,7 +68,7 @@ try {
 
 
 const firehoseSubscription = new FirehoseSubscription(
-    [lieDetectorHandlerController],
+    [lieDetectorHandlerController, magic8BallHandlerController],
     150
 );
 
